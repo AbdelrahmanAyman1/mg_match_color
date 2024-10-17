5cfa586ce86ae74d54e74fa4dd1226c41774515c
abdlerahman ayman <abdelrahmanayman559@gmail.com>
2024-10-17 23:49:14 +0300

    add custom bottom app bar

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mg_match_color/widgets/custom_bottom_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(bottomNavigationBar: CustomBottomAppBar());
   }
 }
